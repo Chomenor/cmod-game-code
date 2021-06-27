bfad6848b8074ca1fbe32bfea9de38962931ca90
Noah Metzger <chomenor@gmail.com>
2021-06-26 22:20:45 -0500

    increase UI arena file list buffer size (ioef)

--- code/ui/ui_gameinfo.c
+++ code/ui/ui_gameinfo.c
@@ -149,7 +149,7 @@ static void UI_LoadArenas( void ) {
 	int			numdirs;
 	vmCvar_t	arenasFile;
 	char		filename[128];
-	char		dirlist[1024];
+	char		dirlist[10240];
 	char*		dirptr;
 	int			i, n;
 	int			dirlen;
@@ -168,7 +168,7 @@ static void UI_LoadArenas( void ) {
 	}
 
 	// get all arenas from .arena files
-	numdirs = trap_FS_GetFileList("scripts", ".arena", dirlist, 1024 );
+	numdirs = trap_FS_GetFileList("scripts", ".arena", dirlist, 10240 );
 	dirptr  = dirlist;
 	for (i = 0; i < numdirs; i++, dirptr += dirlen+1) {
 		dirlen = strlen(dirptr);

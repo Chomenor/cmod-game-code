Chomenor/cmod-game-code

commit dd22048ddc41990a4c804a0a8385daf2fe7877d5

initial fps-neutral physics support

code/game/g_active.c
@@ -484,7 +484,7 @@ void SpectatorThink( gentity_t *ent, usercmd_t *ucmd ) {
 		pm.pointcontents = trap_PointContents;
 
 		// perform a pmove
-		Pmove (&pm);
+		Pmove (&pm, G_PmoveFixedValue());
 
 		// save results of pmove
 		VectorCopy( client->ps.origin, ent->s.origin );
@@ -1662,7 +1662,7 @@ void ClientThink_real( gentity_t *ent ) {
 //		G_Printf("serverTime >>>>>\n" );
 	}
 
-	msec = ucmd->serverTime - client->ps.commandTime;
+	msec = PM_NextMoveTime( client->ps.commandTime, ucmd->serverTime, G_PmoveFixedValue() ) - client->ps.commandTime;
 	// following others may result in bad times, but we still want
 	// to check for follow toggles
 	if ( msec < 1 && client->sess.spectatorState != SPECTATOR_FOLLOW ) {
@@ -1761,7 +1761,7 @@ void ClientThink_real( gentity_t *ent ) {
 	VectorCopy( client->ps.origin, oldOrigin );
 
 	// perform a pmove
-	Pmove (&pm);
+	Pmove (&pm, G_PmoveFixedValue());
 
 	// save results of pmove
 	if ( ent->client->ps.eventSequence != oldEventSequence ) {

code/cgame/cg_local.h
@@ -1062,6 +1062,11 @@ typedef struct {
 	qhandle_t	corner_ll_8_30;
 } cgMedia_t;
 
+// These values are loaded from server mod configstrings.
+typedef struct {
+	// player movement
+	int pMoveFixed;
+} modConfig_t;
 
 // The client game static (cgs) structure hold everything
 // loaded or calculated from the gamestate.  It will NOT
@@ -1129,6 +1134,10 @@ typedef struct {
 
 	//objectives
 	objective_t	objectives[MAX_OBJECTIVES];
+
+	// config info
+	modConfig_t modConfig;
+	qboolean modConfigSet[MAX_CONFIGSTRINGS];
 } cgs_t;
 
 //==============================================================================
@@ -1566,6 +1575,7 @@ void CG_InitConsoleCommands( void );
 //
 void CG_ExecuteNewServerCommands( int latestSequence );
 void CG_ParseServerinfo( void );
+void CG_ParseModConfig( void );
 void CG_SetConfigValues( void );
 
 //

code/cgame/cg_main.c
@@ -1167,6 +1167,8 @@ void CG_Init( int serverMessageNum, int serverCommandSequence ) {
 
 	CG_ParseServerinfo();
 
+	CG_ParseModConfig();
+
 	// load the new map
 	CG_LoadingString( "collision map" );
 

code/cgame/cg_predict.c
@@ -14,6 +14,10 @@ static	centity_t	*cg_solidEntities[MAX_ENTITIES_IN_SNAPSHOT];
 static	int			cg_numTriggerEntities;
 static	centity_t	*cg_triggerEntities[MAX_ENTITIES_IN_SNAPSHOT];
 
+// reference point for prediction error decay
+static	int			lastMoveTime = -1;
+static	vec3_t		lastMoveOrigin;
+
 /*
 ====================
 CG_BuildSolidList
@@ -422,6 +426,47 @@ static void CG_TouchTriggerPrediction( void ) {
 	}
 }
 
+/*
+=================
+CG_HaveStepEvent
+=================
+*/
+static qboolean CG_HaveStepEvent( playerState_t *ps ) {
+	int i;
+
+	for ( i = ps->eventSequence - MAX_PS_EVENTS; i < ps->eventSequence; ++i ) {
+		if ( i >= 0 ) {
+			int event = ps->events[i & ( MAX_PS_EVENTS - 1 )];
+			if ( event == EV_STEP_4 || event == EV_STEP_8 || event == EV_STEP_12 || event == EV_STEP_16 )
+				return qtrue;
+		}
+	}
+
+	return qfalse;
+}
+
+/*
+=================
+CG_FinalPredict
+=================
+*/
+static void CG_FinalPredict( playerState_t *ps, usercmd_t *cmd ) {
+	playerState_t temp = *ps;
+	temp.eventSequence = 0;
+
+	cg_pmove.cmd = *cmd;
+	cg_pmove.ps = &temp;
+	Pmove( &cg_pmove, 0 );
+
+	ps->origin[0] = temp.origin[0];
+	ps->origin[1] = temp.origin[1];
+	if ( !CG_HaveStepEvent( &temp ) ) {
+		ps->origin[2] = temp.origin[2];
+	}
+
+	VectorCopy( temp.viewangles, ps->viewangles );
+	ps->bobCycle = temp.bobCycle;
+}
 
 /*
 =================
@@ -563,57 +608,52 @@ void CG_PredictPlayerState( void ) {
 		// from the snapshot, but on a wan we will have
 		// to predict several commands to get to the point
 		// we want to compare
-		if ( cg.predictedPlayerState.commandTime == oldPlayerState.commandTime ) {
+		if ( cg.predictedPlayerState.commandTime == lastMoveTime && !cg.thisFrameTeleport ) {
 			vec3_t	delta;
 			float	len;
+			vec3_t	adjusted;
 
-			if ( cg.thisFrameTeleport ) {
-				// a teleport will not cause an error decay
-				VectorClear( cg.predictedError );
+			// make sure the error decay is only run once per frame, even if pmove sets the same
+			// commandTime for multiple usercmds due to fixed frame lengths
+			lastMoveTime = -1;
+
+			CG_AdjustPositionForMover( cg.predictedPlayerState.origin,
+				cg.predictedPlayerState.groundEntityNum, cg.physicsTime, cg.oldTime, adjusted );
+
+			if ( cg_showmiss.integer ) {
+				if (!VectorCompare( lastMoveOrigin, adjusted )) {
+					CG_Printf("prediction error\n");
+				}
+			}
+			VectorSubtract( lastMoveOrigin, adjusted, delta );
+			len = VectorLength( delta );
+			if ( len > 0.1 ) {
 				if ( cg_showmiss.integer ) {
-					CG_Printf( "PredictionTeleport\n" );
+					CG_Printf("Prediction miss: %f\n", len);
 				}
-				cg.thisFrameTeleport = qfalse;
-			} else {
-				vec3_t	adjusted;
-				CG_AdjustPositionForMover( cg.predictedPlayerState.origin,
-					cg.predictedPlayerState.groundEntityNum, cg.physicsTime, cg.oldTime, adjusted );
+				if ( cg_errorDecay.integer ) {
+					int		t;
+					float	f;
 
-				if ( cg_showmiss.integer ) {
-					if (!VectorCompare( oldPlayerState.origin, adjusted )) {
-						CG_Printf("prediction error\n");
+					t = cg.time - cg.predictedErrorTime;
+					f = ( cg_errorDecay.value - t ) / cg_errorDecay.value;
+					if ( f < 0 ) {
+						f = 0;
 					}
+					if ( f > 0 && cg_showmiss.integer ) {
+						CG_Printf("Double prediction decay: %f\n", f);
+					}
+					VectorScale( cg.predictedError, f, cg.predictedError );
+				} else {
+					VectorClear( cg.predictedError );
 				}
-				VectorSubtract( oldPlayerState.origin, adjusted, delta );
-				len = VectorLength( delta );
-				if ( len > 0.1 ) {
-					if ( cg_showmiss.integer ) {
-						CG_Printf("Prediction miss: %f\n", len);
-					}
-					if ( cg_errorDecay.integer ) {
-						int		t;
-						float	f;
-
-						t = cg.time - cg.predictedErrorTime;
-						f = ( cg_errorDecay.value - t ) / cg_errorDecay.value;
-						if ( f < 0 ) {
-							f = 0;
-						}
-						if ( f > 0 && cg_showmiss.integer ) {
-							CG_Printf("Double prediction decay: %f\n", f);
-						}
-						VectorScale( cg.predictedError, f, cg.predictedError );
-					} else {
-						VectorClear( cg.predictedError );
-					}
-					VectorAdd( delta, cg.predictedError, cg.predictedError );
-					cg.predictedErrorTime = cg.oldTime;
-				}
+				VectorAdd( delta, cg.predictedError, cg.predictedError );
+				cg.predictedErrorTime = cg.oldTime;
 			}
 		}
 
 
-		Pmove (&cg_pmove);
+		Pmove (&cg_pmove, cgs.modConfig.pMoveFixed);
 
 		moved = qtrue;
 
@@ -621,15 +661,28 @@ void CG_PredictPlayerState( void ) {
 		CG_TouchTriggerPrediction();
 	}
 
+	if ( cg.thisFrameTeleport ) {
+		// reset error decay on teleport
+		VectorClear( cg.predictedError );
+		if ( cg_showmiss.integer ) {
+			CG_Printf( "PredictionTeleport\n" );
+		}
+		cg.thisFrameTeleport = qfalse;
+	}
+
+	// set error decay markers for next frame
+	lastMoveTime = cg.predictedPlayerState.commandTime;
+	CG_AdjustPositionForMover( cg.predictedPlayerState.origin,
+		cg.predictedPlayerState.groundEntityNum,
+		cg.physicsTime, cg.time, lastMoveOrigin );
+
 	if ( cg_showmiss.integer > 1 ) {
 		CG_Printf( "[%i : %i] ", cg_pmove.cmd.serverTime, cg.time );
 	}
 
-	if ( !moved ) {
-		if ( cg_showmiss.integer ) {
-			CG_Printf( "not moved\n" );
-		}
-		return;
+	// simulate any movement fragment left over from main move due to fixed frame length
+	if ( latestCmd.serverTime > cg.predictedPlayerState.commandTime ) {
+		CG_FinalPredict( &cg.predictedPlayerState, &latestCmd );
 	}
 
 	// adjust for the movement of the groundentity

code/cgame/cg_servercmds.c
@@ -126,6 +126,48 @@ void CG_ParseServerinfo( void ) {
 
 /*
 ==================
+CG_ParseModConfig
+
+The mod config format provides a standard method for servers to send mod configuration
+data to clients. Any configstring index can be used to send mod config values, to allow
+the server more flexibility to avoid conflicts or optimize. In the case of duplicate
+keys the numerically higher configstring index takes precedence.
+
+Configstrings containing mod config data begin with the prefix "!modcfg ", followed by
+a standard slash-separated info string similar to serverinfo or systeminfo.
+==================
+*/
+void CG_ParseModConfig( void ) {
+	int i;
+	char key[BIG_INFO_STRING];
+	char value[BIG_INFO_STRING];
+
+	memset( &cgs.modConfig, 0, sizeof( cgs.modConfig ) );
+	memset( cgs.modConfigSet, 0, sizeof( cgs.modConfigSet ) );
+
+	// look for any configstring matching "!modcfg " prefix
+	for ( i = 0; i < MAX_CONFIGSTRINGS; ++i ) {
+		const char *str = CG_ConfigString( i );
+
+		if ( str[0] == '!' && !Q_stricmpn( str, "!modcfg ", 8 ) && strlen( str ) < BIG_INFO_STRING ) {
+			const char *cur = &str[8];
+
+			// load values
+			while ( 1 ) {
+				Info_NextPair( &cur, key, value );
+				if ( !key[0] )
+					break;
+				if ( !Q_stricmp( key, "pMoveFixed" ) )
+					cgs.modConfig.pMoveFixed = atoi( value );
+			}
+
+			cgs.modConfigSet[i] = qtrue;
+		}
+	}
+}
+
+/*
+==================
 CG_ParseWarmup
 ==================
 */
@@ -184,6 +226,11 @@ static void CG_ConfigStringModified( void ) {
 
 	// look up the individual string that was modified
 	str = CG_ConfigString( num );
+
+	// check if a mod config string was changed
+	if ( !Q_stricmpn( str, "!modcfg ", 8 ) || cgs.modConfigSet[num] ) {
+		CG_ParseModConfig();
+	}
 
 	// do something with it if necessary
 	if ( num == CS_MUSIC ) {

code/common/bg_pmove.c
@@ -2242,6 +2242,45 @@ void PmoveSingle (pmove_t *pmove) {
 	SnapVector( pm->ps->velocity );
 }
 
+/*
+================
+PM_NextFrameBreak
+
+Given a current command time, returns next valid frame end time according to
+fixed length settings. Returned value will always be higher than input.
+================
+*/
+static int PM_NextFrameBreak( int commandTime, int fixedLength ) {
+	return ( commandTime + fixedLength ) - ( commandTime % fixedLength );
+}
+
+/*
+================
+PM_NextMoveTime
+
+Given a current and target time, returns next time to move player.
+Returns currentTime if no move is needed, otherwise returns a value greater than currentTime.
+================
+*/
+int PM_NextMoveTime( int currentTime, int targetTime, int pMoveFixed ) {
+	if ( currentTime > targetTime ) {
+		return currentTime;
+	}
+
+	if ( pMoveFixed > 0 && pMoveFixed < 35 ) {
+		int nextTime = PM_NextFrameBreak( currentTime, pMoveFixed );
+		if ( nextTime > targetTime ) {
+			return currentTime;
+		}
+		return nextTime;
+	}
+
+	if ( targetTime > currentTime + 66 ) {
+		targetTime = currentTime + 66;
+	}
+
+	return targetTime;
+}
 
 /*
 ================
@@ -2250,31 +2289,23 @@ Pmove
 Can be called by either the server or the client
 ================
 */
-void Pmove (pmove_t *pmove) {
-	int			finalTime;
+void Pmove( pmove_t *pmove, int pMoveFixed ) {
+	usercmd_t inputCmd = pmove->cmd;
 
-	finalTime = pmove->cmd.serverTime;
-
-	if ( finalTime < pmove->ps->commandTime ) {
-		return;	// should not happen
+	if ( inputCmd.serverTime > pmove->ps->commandTime + 1000 ) {
+		pmove->ps->commandTime = inputCmd.serverTime - 1000;
 	}
 
-	if ( finalTime > pmove->ps->commandTime + 1000 ) {
-		pmove->ps->commandTime = finalTime - 1000;
-	}
-
-	// chop the move up if it is too long, to prevent framerate
-	// dependent behavior
-	while ( pmove->ps->commandTime != finalTime ) {
-		int		msec;
-
-		msec = finalTime - pmove->ps->commandTime;
-
-		if ( msec > 66 ) {
-			msec = 66;
+	while( 1 ) {
+		int nextTime = PM_NextMoveTime( pmove->ps->commandTime, inputCmd.serverTime, pMoveFixed );
+		if ( nextTime <= pmove->ps->commandTime ) {
+			break;
 		}
-		pmove->cmd.serverTime = pmove->ps->commandTime + msec;
-		PmoveSingle( pmove );
-	}
 
+		pmove->cmd.serverTime = nextTime;
+		PmoveSingle( pmove );
+
+		// reset any changes to command during move
+		pmove->cmd = inputCmd;
+	}
 }

code/common/bg_public.h
@@ -64,6 +64,11 @@
 #error overflow: (CS_MAX) > MAX_CONFIGSTRINGS
 #endif
 
+// Arbitrary index that the game module uses to write mod config info. Clients will look for
+// mod config prefix in all configstrings, so this can be changed to any non-conflicting value
+// without affecting the client.
+#define CS_MOD_CONFIG		870
+
 typedef enum {
 	GT_FFA,				// free for all
 	GT_TOURNAMENT,		// one on one tournament
@@ -155,7 +160,8 @@ typedef struct {
 
 // if a full pmove isn't done on the client, you can just update the angles
 void PM_UpdateViewAngles( playerState_t *ps, const usercmd_t *cmd );
-void Pmove (pmove_t *pmove);
+int PM_NextMoveTime( int currentTime, int targetTime, int pMoveFixed );
+void Pmove( pmove_t *pmove, int pMoveFixed );
 
 //===================================================================================
 

code/game/g_local.h
@@ -569,6 +569,7 @@ static void G_SayTo( gentity_t *ent, gentity_t *other, int mode, int color, cons
 //
 // g_main.c
 //
+int G_PmoveFixedValue( void );
 void FindIntermissionPoint( void );
 void G_RunThink (gentity_t *ent);
 void SendScoreboardMessageToAllClients( void );
@@ -790,6 +791,9 @@ extern  vmCvar_t	g_team_group_blue;
 extern	vmCvar_t	g_random_skin_limit;
 extern	vmCvar_t	g_noJoinTimeout;
 extern	vmCvar_t	g_classChangeDebounceTime;
+
+extern	vmCvar_t	g_pMoveFixed;
+extern	vmCvar_t	g_pMoveMsec;
 
 void	trap_Printf( const char *fmt );
 void	trap_Error( const char *fmt );

commit ae3e1cabf6bb4878a0ca5168d20cfc7f3e755048

fix ClientTimerActions timing bug introduced in dd22048ddc41990a4c804a0a8385daf2fe7877d5

code/game/g_active.c
@@ -1637,6 +1637,7 @@ once for each server frame, which makes for smooth demo recording.
 ==============
 */
 void ClientThink_real( gentity_t *ent ) {
+	int			oldCommandTime = ent->client->ps.commandTime;
 	gclient_t	*client;
 	pmove_t		pm;
 	vec3_t		oldOrigin;
@@ -1663,14 +1664,11 @@ void ClientThink_real( gentity_t *ent ) {
 //		G_Printf("serverTime >>>>>\n" );
 	}
 
-	msec = PM_NextMoveTime( client->ps.commandTime, ucmd->serverTime, G_PmoveFixedValue() ) - client->ps.commandTime;
-	// following others may result in bad times, but we still want
-	// to check for follow toggles
-	if ( msec < 1 && client->sess.spectatorState != SPECTATOR_FOLLOW ) {
+	if ( !PM_IsMoveNeeded( client->ps.commandTime, ucmd->serverTime, G_PmoveFixedValue() ) &&
+			// following others may result in bad times, but we still want
+			// to check for follow toggles
+			client->sess.spectatorState != SPECTATOR_FOLLOW ) {
 		return;
-	}
-	if ( msec > 200 ) {
-		msec = 200;
 	}
 
 	//
@@ -1849,6 +1847,10 @@ void ClientThink_real( gentity_t *ent ) {
 	}
 
 	// perform once-a-second actions
+	msec = client->ps.commandTime - oldCommandTime;
+	if ( msec > 200 ) {
+		msec = 200;
+	}
 	ClientTimerActions( ent, msec );
 
 	if ( ent->client->teleportTime > 0 && ent->client->teleportTime < level.time )
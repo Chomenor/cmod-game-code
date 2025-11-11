5064d8598e817056d585e50bf7cc4bf4e69cc86f
Noah Metzger <chomenor@gmail.com>
2025-11-10 18:52:23 -0600

    fix incorrect joined game messages being displayed
    Fixes issue causing joined game messages to be incorrectly generated for spectators since 2ee5447c79ac86d00dcc4cd9ceff33333e181393

--- code/game/g_active.c
+++ code/game/g_active.c
@@ -2027,8 +2027,8 @@ void SpectatorClientEndFrame( gentity_t *ent ) {
 			} else {
 				// drop them to free spectators unless they are dedicated camera followers
 				if ( ent->client->sess.spectatorClient >= 0 ) {
-					ent->client->sess.spectatorState = SPECTATOR_FREE;
-					ClientBegin( ent->client - level.clients );
+					StopFollowing( ent );
+					ClientSpawn( ent, CST_RESPAWN );
 				}
 			}
 		}
